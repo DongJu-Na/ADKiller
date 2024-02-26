d008e1086e1413a52e0452ba18a4f0e3ea7a0d82
DongJu-Na <79893048+DongJu-Na@users.noreply.github.com>
2024-02-26 18:06:19 +0900

    init v6
    불필요 주석 제거

--- service-worker-utils.js
+++ service-worker-utils.js
@@ -9,14 +9,12 @@ console.info("service-worker-utils.js load");
         if (message.type === 'bannerAdBlock') {
             console.log('Received bannerAdBlock value:', message.value);
 
-            // 여기에 저장 로직을 추가할 수 있습니다.
             setStorage({ bannerAdBlock: message.value })
                 .then(() => console.log('Banner Ad Block value saved'))
                 .catch(error => console.error('Error saving bannerAdBlock value:', error));
         } else if (message.type === 'videoAdBlock') {
             console.log('Received videoAdBlock value:', message.value);
 
-            // 여기에 저장 로직을 추가할 수 있습니다.
             setStorage({ videoAdBlock: message.value })
                 .then(() => console.log('Video Ad Block value saved'))
                 .catch(error => console.error('Error saving videoAdBlock value:', error));
